f43d311b025f3308b354b7f0b2bd2219cf887cbc
sksat <sksat@sksat.net>
2021-09-02 14:17:12 +0900

    u

--- entrypoint.py
+++ entrypoint.py
@@ -3,7 +3,10 @@ import shutil
 from importlib import import_module
 
 script_path = sys.argv[1]
+print("loading script from " + script_path)
 shutil.copy(script_path, "./script.py")
 
 script = import_module("script")
+
+print("execure script")
 script.generate(sys.argv[2])
--- test/hello.py
+++ test/hello.py
@@ -4,6 +4,7 @@ from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import A4
 
 def generate(output):
+    print("hello")
     c = canvas.Canvas(output)
     c.drawString(100, 100, "hello")
     c.save()
